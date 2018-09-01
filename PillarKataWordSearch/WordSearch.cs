75773b4f36af8f2d7058f3571d030bba3386dd6b
Barret Blake <barretblake@Live.com>
2018-08-31 22:29:21 -0400

    class initial data setup

--- PillarKataWordSearch/WordSearch.cs
+++ PillarKataWordSearch/WordSearch.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace PillarKataWordSearch
 {
-   public class WordSearch
+    public class WordSearch
     {
+        private List<string> searchWords;
+        private char[,] letterGrid;
 
+        public WordSearch(string searchWordsInput, int blockSize, List<string> letterGridList)
+        {
+            letterGrid = new char[blockSize, blockSize];
+            searchWords = searchWordsInput.Split(',').ToList();
+            for (var x = 0; x < blockSize; x++)
+            {
+                var listRow = letterGridList[x].Split(',');
+                for (var y = 0; y < blockSize; y++)
+                {
+                    letterGrid[x, y] = listRow[y].FirstOrDefault();
+                }
+            }
+        }
     }
 }
