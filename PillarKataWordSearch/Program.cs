9f905a1f6990e2a1bc250f80c98e8098b80bfc37
Barret Blake <barretblake@Live.com>
2018-09-03 13:30:06 -0400

    Adding README directions

--- PillarKataWordSearch/Program.cs
+++ PillarKataWordSearch/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace PillarKataWordSearch
 {
@@ -8,10 +10,48 @@ namespace PillarKataWordSearch
         static void Main(string[] args)
         {
             var file = args[0];
+            string searchWords;
+                var searchGrid = new List<string>();
+            int size;
+
             using (var stream = new StreamReader(file))
             {
+                searchWords = stream.ReadLine();
+                if (string.IsNullOrEmpty(searchWords))
+                {
+                    Console.WriteLine("INVALID DATA FILE - FIRST ROW EMPTY");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
 
+                var line = stream.ReadLine();
+                while (null != line)
+                {
+                    searchGrid.Add(line);
+                    line = stream.ReadLine();
+                }
+
+                var columns = searchGrid.First().Split(',').Length;
+                var rows = searchGrid.Count;
+                if (columns != rows)
+                {
+                    Console.WriteLine("INVALID DATA FILE - LETTER GRID IS NOT SQUARE");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+                size = rows;
             }
+
+            var search = new WordSearch(size, searchGrid);
+            var wordList = searchWords.Split(',').ToList();
+            var results = search.SearchWords(wordList);
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
 
             Console.ReadLine();
         }
---
+++ README.md
@@ -0,0 +1,10 @@
+## Word Search Code Kata for Pillar Technology
+##### https://github.com/PillarTechnology/kata-word-search
+
+#### Solution Author: Barret Blake
+
+### Instructions:
+This is a C# .NET Core v2.1 solution. It must be opened on a machine with Visual Studio 2017 or better and .NET Core 2.1 or better installed.
+
+#### Build In Visual Studio (easiest):
+1. 
